6e9252eec2ccc73a9053c2ebb810f75edd651d32
adrian <adrianruizxerez@gmail.com>
2022-04-18 14:55:00 +0200

    commitTest0002

--- frontend/src/app/game/character/knight.ts
+++ frontend/src/app/game/character/knight.ts
@@ -9,16 +9,20 @@ export default class Knight extends Phaser.Physics.Arcade.Sprite {
     private shiftKey!: any;
     private spaceKey!: any;
 
+    //Actions Booleans
+    private attackCD = true;
     private allowMove = true;
 
-    private escena!: Phaser.Scene;
+    private currentScene!: Phaser.Scene;
+
+
 
     constructor(config: any) {
-        super(config.escena, config.x, config.y, config.texture);
+        super(config.currentScene, config.x, config.y, config.texture);
 
-        this.escena = config.escena;
-        this.escena.physics.world.enable(this);
-        this.escena.add.existing(this);
+        this.currentScene = config.currentScene;
+        this.currentScene.physics.world.enable(this);
+        this.currentScene.add.existing(this);
 
 
         this.body.setSize(20, 38);
@@ -33,17 +37,84 @@ export default class Knight extends Phaser.Physics.Arcade.Sprite {
         );
 
 
-        this.cursors = this.escena.input.keyboard.createCursorKeys();
-        this.WASD = this.escena.input.keyboard.addKeys('W,A,S,D');
-        this.shiftKey = this.escena.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
-        this.spaceKey = this.escena.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.cursors = this.currentScene.input.keyboard.createCursorKeys();
+        this.WASD = this.currentScene.input.keyboard.addKeys('W,A,S,D');
+        this.shiftKey = this.currentScene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
+        this.spaceKey = this.currentScene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-        // this.play(Constants.PLAYER.ANIMATION.IDLE);
+        this.play(Constants.PLAYER.ANIMATION.IDLE);
+
 
     }
 
+    create() {
+        this.anims.create({
+            key: 'idle',
+            frames: this.currentScene.anims.generateFrameNumbers('knight', { start: 0, end: 9 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: 'left',
+            frames: this.currentScene.anims.generateFrameNumbers('run', { start: 0, end: 9 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: 'idle',
+            frames: this.currentScene.anims.generateFrameNumbers('knight', { start: 0, end: 9 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: 'left',
+            frames: this.currentScene.anims.generateFrameNumbers('run', { start: 0, end: 9 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: 'jump',
+            frames: this.currentScene.anims.generateFrameNumbers('jump', { start: 0, end: 2 }),
+            frameRate: 10,
+        });
+        this.anims.create({
+            key: 'fall',
+            frames: this.currentScene.anims.generateFrameNumbers('fall', { start: 0, end: 2 }),
+            frameRate: 10,
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.currentScene.anims.generateFrameNumbers('run', { start: 0, end: 9 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: 'dash',
+            frames: this.anims.generateFrameNumbers('dash', { start: 0, end: 1 }),
+            frameRate: 30
+        });
+
+        this.anims.create({
+            key: 'slide',
+            frames: this.currentScene.anims.generateFrameNumbers('slide', { start: 0, end: 3 }),
+            frameRate: 60
+        });
+
+        this.anims.create({
+            key: 'downSwing',
+            frames: this.currentScene.anims.generateFrameNumbers('downAttack', { start: 0, end: 3 }),
+            frameRate: 20,
+        });
+    }
 
     override update() {
+
+
+
         //Moveset
         if (this.allowMove) {
             // <== Left
@@ -102,25 +173,32 @@ export default class Knight extends Phaser.Physics.Arcade.Sprite {
                 this.anims.stop();
                 this.setVelocityY(-300);
                 this.anims.play('jump');
+                // this.cooldown++
 
             }
             if (this.body.velocity.y >= 0 && !this.body.blocked.down) {
                 this.anims.stop();
                 this.anims.play('fall');
             }
-            if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
+
+
+            if (Phaser.Input.Keyboard.JustDown(this.spaceKey) && this.attackCD) {
+                this.setCooldown(5000, this.attackCD)
                 this.setVelocityX(0);
                 this.anims.stop();
                 this.blockMove(300)
                 this.anims.play('downSwing');
+
 
             }
 
         }
 
 
-        //Right Dash
-        if (Phaser.Input.Keyboard.JustDown(this.shiftKey) && this.body.blocked.down && (this.cursors.right.isDown || this.cursors.left.isDown || this.WASD.A.isDown || this.WASD.D.isDown)) {
+        //Slide
+        if (Phaser.Input.Keyboard.JustDown(this.shiftKey) && this.body.blocked.down &&
+            (this.cursors.right.isDown || this.cursors.left.isDown
+                || this.WASD.A.isDown || this.WASD.D.isDown)) {
             this.anims.stop();
             this.blockMove(500)
             if (this.flipX) {
@@ -138,9 +216,23 @@ export default class Knight extends Phaser.Physics.Arcade.Sprite {
     blockMove(timeMs: number) {
         this.allowMove = false
 
-        this.escena.time.delayedCall(timeMs, () => {
+        this.currentScene.time.delayedCall(timeMs, () => {
 
             this.allowMove = true
+        }, [], this)
+    }
+
+    setCooldown(timeMs: number, action: boolean) {
+        console.log("hola hola");
+        console.log(`accion entrante ${action}`);
+
+
+        action = false
+        console.log(`accion puesta en negativo ${action}`);
+        this.currentScene.time.delayedCall(timeMs, () => {
+
+            action = true
+            console.log(`accion vuelta a verdadero ${action}`);
         }, [], this)
     }
 }
--- frontend/src/app/game/scenes/Scene1.ts
+++ frontend/src/app/game/scenes/Scene1.ts
@@ -6,13 +6,6 @@ import Constants from '../Constants';
 
 export class Scene1 extends Phaser.Scene {
   // Propiedades
-  // private ground!: any;
-  // private knight!: any;
-  // private cursors!: any;
-  // private allowMove = true;
-  // private shiftKey!: any;
-  // private spaceKey!: any;
-
   private player!: Knight;
 
   private width!: number;
@@ -35,20 +28,14 @@ export class Scene1 extends Phaser.Scene {
   preload() { }
 
   create() {
-
-
     //background layer 1
     this.createBackground(this, 'forestBckgr-1', 12, 0)
 
     //background layer 2
-
     this.createBackground(this, 'forestBckgr-2', 12, 0.5)
-
-
 
     //background layer 3
     this.createBackground(this, 'forestBckgr-3', 14, 1)
-
 
     //load tile map
     this.tileMap = this.make.tilemap({ key: Constants.MAPS.LEVEL1.TILEMAPJSON, tileWidth: 16, tileHeight: 16 });
@@ -57,12 +44,13 @@ export class Scene1 extends Phaser.Scene {
     //Player
     this.tileMap.findObject(Constants.PLAYER.ID, (d: any) => {
       this.player = new Knight({
-        escena: this,
+        currentScene: this,
         x: d.x,
         y: d.y,
         texture: Constants.PLAYER.ID
       });
     });
+    this.player.create();
 
     //camera
     this.cameras.main.setBounds(0, 0, this.tileMap.widthInPixels, this.tileMap.heightInPixels);
@@ -74,71 +62,60 @@ export class Scene1 extends Phaser.Scene {
     this.tileMapLayer = this.tileMap.createLayer(Constants.MAPS.LEVEL1.PLATAFORMLAYER, this.tileSet);
     this.tileMapLayer.setCollisionByExclusion([-1]);
 
-
     this.physics.add.collider(this.player, this.tileMapLayer);
 
 
 
-
-
-
-
-
-
-
-
-
-
     //Animaciones
-    this.anims.create({
-      key: 'idle',
-      frames: this.anims.generateFrameNumbers('knight', { start: 0, end: 9 }),
-      frameRate: 20,
-      repeat: -1,
-    });
+    // this.anims.create({
+    //   key: 'idle',
+    //   frames: this.anims.generateFrameNumbers('knight', { start: 0, end: 9 }),
+    //   frameRate: 20,
+    //   repeat: -1,
+    // });
 
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('run', { start: 0, end: 9 }),
-      frameRate: 20,
-      repeat: -1,
-    });
+    // this.anims.create({
+    //   key: 'left',
+    //   frames: this.anims.generateFrameNumbers('run', { start: 0, end: 9 }),
+    //   frameRate: 20,
+    //   repeat: -1,
+    // });
 
-    this.anims.create({
-      key: 'jump',
-      frames: this.anims.generateFrameNumbers('jump', { start: 0, end: 2 }),
-      frameRate: 10,
-    });
-    this.anims.create({
-      key: 'fall',
-      frames: this.anims.generateFrameNumbers('fall', { start: 0, end: 2 }),
-      frameRate: 10,
-    });
+    // this.anims.create({
+    //   key: 'jump',
+    //   frames: this.anims.generateFrameNumbers('jump', { start: 0, end: 2 }),
+    //   frameRate: 10,
+    // });
+    // this.anims.create({
+    //   key: 'fall',
+    //   frames: this.anims.generateFrameNumbers('fall', { start: 0, end: 2 }),
+    //   frameRate: 10,
+    // });
 
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('run', { start: 0, end: 9 }),
-      frameRate: 20,
-      repeat: -1,
-    });
+    // this.anims.create({
+    //   key: 'right',
+    //   frames: this.anims.generateFrameNumbers('run', { start: 0, end: 9 }),
+    //   frameRate: 20,
+    //   repeat: -1,
+    // });
 
-    this.anims.create({
-      key: 'dash',
-      frames: this.anims.generateFrameNumbers('dash', { start: 0, end: 1 }),
-      frameRate: 30
-    });
+    // this.anims.create({
+    //   key: 'dash',
+    //   frames: this.anims.generateFrameNumbers('dash', { start: 0, end: 1 }),
+    //   frameRate: 30
+    // });
 
-    this.anims.create({
-      key: 'slide',
-      frames: this.anims.generateFrameNumbers('slide', { start: 0, end: 3 }),
-      frameRate: 60
-    });
+    // this.anims.create({
+    //   key: 'slide',
+    //   frames: this.anims.generateFrameNumbers('slide', { start: 0, end: 3 }),
+    //   frameRate: 60
+    // });
 
-    this.anims.create({
-      key: 'downSwing',
-      frames: this.anims.generateFrameNumbers('downAttack', { start: 0, end: 3 }),
-      frameRate: 20,
-    });
+    // this.anims.create({
+    //   key: 'downSwing',
+    //   frames: this.anims.generateFrameNumbers('downAttack', { start: 0, end: 3 }),
+    //   frameRate: 20,
+    // });
 
   }
 
