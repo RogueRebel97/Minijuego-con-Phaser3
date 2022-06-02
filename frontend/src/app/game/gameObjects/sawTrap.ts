581824ca9431c474472e1ccd899623cf726cc1d9
adrian <adrianruizxerez@gmail.com>
2022-06-02 14:09:01 +0200

    last commit

--- frontend/src/app/game/gameObjects/sawTrap.ts
+++ frontend/src/app/game/gameObjects/sawTrap.ts
@@ -8,28 +8,28 @@ export default class SawTrap extends Phaser.Physics.Arcade.Sprite {
     //Current Game Scene
     private currentScene!: Phaser.Scene
 
+    private damage: number = 45
+    private force: number = 300
+
 
     constructor(config: any, angle?: number) {
         super(config.currentScene, config.x, config.y, config.texture);
 
+
+
         if (config.angle) {
             this.angle = config.angle
-            console.log(config.angle);
+
         }
 
         this.currentScene = config.currentScene
 
         // Physics
-        // this.currentScene.physics.world.enable(this)
-        this.currentScene.add.existing(this)
+        this.currentScene.add.existing(this);
 
 
 
 
-
-
-
-        this.create()
 
     }
 
@@ -44,6 +44,31 @@ export default class SawTrap extends Phaser.Physics.Arcade.Sprite {
             repeat: -1,
         });
 
+        this.setCircle(13)
+
+        if (this.angle == 360 || this.angle == 0) {
+            // console.log(this.angle);
+            this.setOffset(this.body.offset.x + 3, this.height * 0.5 - 25)
+        }
+        else if (this.angle == 180 || this.angle == -180) {
+            this.setOffset(this.body.offset.x + 3, this.height * 0.5)
+        } else if (this.angle == 90) {
+            // console.log(this.angle);
+
+            this.setOffset(this.body.offset.x + 16, this.height * 0.5 - 13)
+        } else if (this.angle == -90) {
+            // console.log(this.angle);
+            this.setOffset(this.body.offset.x - 10, this.height * 0.5 - 13)
+        } else if (this.angle == 135 || this.angle == -135) {
+            console.log("aaaa");
+
+            console.log(this.angle);
+            this.setOffset(this.body.offset.x + 13, this.height * 0.5 - 5)
+        } else {
+            console.log(this.angle);
+        }
+
+
 
 
 
@@ -51,6 +76,14 @@ export default class SawTrap extends Phaser.Physics.Arcade.Sprite {
 
     override update() {
         this.anims.play('saw', true)
+    }
+
+    getDmg() {
+        return this.damage
+    }
+
+    getForce() {
+        return this.force
     }
 }
 
